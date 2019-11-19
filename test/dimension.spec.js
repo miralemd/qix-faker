a4d3bd701ee4dad449418d1db724ed1527ffe56c
Miralem Drek <mm85dk@gmail.com>
2019-11-19 22:00:23 +0100

    feat: add qState

--- test/dimension.spec.js
+++ test/dimension.spec.js
@@ -1,21 +1,30 @@
 const dimension = require('../lib/dimension');
 
 describe('dimension', () => {
+  const faker = {
+    random: {
+      arrayElement: () => 'O',
+    },
+  };
+
   it('should provide value() with row index', () => {
     const d = dimension({
       value: idx => `a:${idx}`,
+      faker,
     });
 
     expect(d.cell(3)).to.eql({
       qElemNumber: 0,
       qText: 'a:3',
       qNum: 3,
+      qState: 'O',
     });
   });
 
   it('should track unique values', () => {
     const d = dimension({
       value: idx => `a-${idx}`,
+      faker,
     });
 
     d.cell(2);
@@ -33,12 +42,14 @@ describe('dimension', () => {
     const d = dimension({
       value: idx => `d:${idx * 3}`,
       attrDims: [ad],
+      faker,
     });
 
     expect(d.cell(2)).to.eql({
       qElemNumber: 0,
       qText: 'd:6',
       qNum: 6,
+      qState: 'O',
       qAttrDims: {
         qValues: [{ qElemNo: 0, qText: 'a:10' }],
       },
@@ -51,12 +62,14 @@ describe('dimension', () => {
     const d = dimension({
       value: idx => `d:${idx * 3}`,
       attrExps: [ae],
+      faker,
     });
 
     expect(d.cell(2)).to.eql({
       qElemNumber: 0,
       qText: 'd:6',
       qNum: 6,
+      qState: 'O',
       qAttrExps: {
         qValues: [{ qText: '$2' }],
       },
@@ -79,6 +92,7 @@ describe('dimension', () => {
       maxCardinalRatio,
       numRows,
       value: idx => `a-${idx}`,
+      faker,
     });
 
     const values = [];
@@ -103,6 +117,7 @@ describe('dimension', () => {
       numRows,
       forceUnique: true,
       value,
+      faker,
     });
 
     const values = [];
@@ -114,9 +129,76 @@ describe('dimension', () => {
     expect(values).to.eql(['a', 'b', false, false]);
   });
 
+  it('should generate all states when qLocked is true', () => {
+    const arrayElement = sinon.stub();
+    arrayElement.onCall(0).returns('L');
+    arrayElement.onCall(1).returns('S');
+    arrayElement.onCall(2).returns('O');
+    arrayElement.onCall(3).returns('D');
+    arrayElement.onCall(4).returns('A');
+    arrayElement.onCall(5).returns('X');
+    arrayElement.onCall(6).returns('XS');
+    arrayElement.onCall(7).returns('XL');
+    arrayElement.onCall(8).returns('S');
+    arrayElement.onCall(9).returns('D');
+
+    const numRows = 10;
+
+    const d = dimension({
+      numRows,
+      value: idx => idx,
+      faker: {
+        random: {
+          arrayElement,
+        },
+      },
+      override: {
+        qLocked: true,
+      },
+    });
+
+    const states = [];
+    for (let r = 0; r < numRows; r++) {
+      states.push(d.cell(r).qState);
+    }
+
+    expect(states).to.eql(['L', 'S', 'O', 'D', 'A', 'X', 'XS', 'XL', 'S', 'D']);
+    expect(arrayElement.firstCall).to.have.been.calledWithExactly(['L', 'S', 'O', 'D', 'A', 'X', 'XS', 'XL']);
+
+    expect(d.info().qStateCounts).to.eql({
+      qAlternative: 1,
+      qDeselected: 2,
+      qExcluded: 1,
+      qLocked: 1,
+      qLockedExcluded: 1,
+      qOption: 1,
+      qSelected: 2,
+      qSelectedExcluded: 1,
+    });
+  });
+
+  it('should not generate locked states when qLocked is falsy', () => {
+    const arrayElement = sinon.stub();
+
+    const d = dimension({
+      numRows: 1,
+      value: idx => idx,
+      faker: {
+        random: {
+          arrayElement,
+        },
+      },
+    });
+
+    d.cell(0);
+
+    expect(arrayElement.firstCall).to.have.been.calledWithExactly(['S', 'O', 'D', 'A', 'X', 'XS']);
+  });
+
   it('should return info', () => {
     const d = dimension({
       value: idx => `a-${idx}`,
+      faker,
       override: {
         qFallbackTitle: 'dim',
         qLocked: true,
@@ -144,7 +226,16 @@ describe('dimension', () => {
     expect(d.info()).to.eql({
       qApprMaxGlyphCount: 3,
       qCardinal: 2,
-      qStateCounts: {},
+      qStateCounts: {
+        qAlternative: 0,
+        qDeselected: 0,
+        qExcluded: 0,
+        qLocked: 0,
+        qLockedExcluded: 0,
+        qOption: 2,
+        qSelected: 0,
+        qSelectedExcluded: 0,
+      },
       qMin: -2,
       qMax: -1,
       qLocked: true,
